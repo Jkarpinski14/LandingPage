690ccce3b14006f49654a4f23ba6a82edb1e031e
Jonah Karpinski <Jkarpinski14@yahoo.com>
2015-05-20 08:30:10 -0700

    Additional edits

--- index.php
+++ index.php
@@ -2,8 +2,11 @@
 <html>
 <head>
 	<title></title>
+	<meta name="viewport" content="width=device width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body>
 
+	<script type="text/javascript" src="js/main.js"></script>
 </body>
 </html>
